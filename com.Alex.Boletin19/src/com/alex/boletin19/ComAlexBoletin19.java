8c8c04b3068a4a1bbe8b28b7c089216bc7fc5145
AlejandroGonzalez02 <agonzalezalonso@danielcastelao.org>
2021-04-26 09:12:58 +0200

    Boletin Realizado

---
+++ com.Alex.Boletin19/src/com/alex/boletin19/ComAlexBoletin19.java
@@ -0,0 +1,17 @@
+package com.alex.boletin19;
+
+/**
+ *
+ * @author sanfa
+ */
+public class ComAlexBoletin19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Xanela xanela=new Xanela();
+        xanela.iniciar();
+    }
+    
+}
